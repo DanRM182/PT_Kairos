20407ec97ad920c48423bc977655d54490e4cfbb
DanRM182 <ddaannn182@gmail.com>
2026-09-23 17:30:21 -0600

    Cliente para buscar en TVMaze

--- src/main/java/com/ramirezmontoya/tvmaze_middleware/client/TvmazeClient.java
+++ src/main/java/com/ramirezmontoya/tvmaze_middleware/client/TvmazeClient.java
@@ -1,5 +1,24 @@
 package com.ramirezmontoya.tvmaze_middleware.client;
 
+import com.ramirezmontoya.tvmaze_middleware.dto.TvmazeShow;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
 @Component
 public class TvmazeClient {
+    private final RestClient restClient;
+
+    public TvmazeClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public List<TvmazeShow> buscarShows(String query) {
+        return restClient.get()
+                .uri("/search/shows?q={q}", query)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<TvmazeShow>>() {});
+    }
 }
