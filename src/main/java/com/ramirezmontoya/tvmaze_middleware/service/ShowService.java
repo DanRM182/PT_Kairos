46073248ec178528c8e69b257be13ff698282b73
DanRM182 <ddaannn182@gmail.com>
2026-09-23 17:58:03 -0600

    Servicio Listo

---
+++ src/main/java/com/ramirezmontoya/tvmaze_middleware/service/ShowService.java
@@ -0,0 +1,23 @@
+package com.ramirezmontoya.tvmaze_middleware.service;
+
+import com.ramirezmontoya.tvmaze_middleware.client.TvmazeClient;
+import com.ramirezmontoya.tvmaze_middleware.dto.ResumenResponse;
+import com.ramirezmontoya.tvmaze_middleware.dto.TvmazeShow;
+import com.ramirezmontoya.tvmaze_middleware.mapper.ShowMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ShowService {
+    private final TvmazeClient tvmazeClient;
+    private final ShowMapper showMapper;
+
+    public List<ResumenResponse> buscar(String query) {
+        return tvmazeClient.buscarShows(query).stream()
+                .map(showMapper::convertirAResponse)
+                .toList();
+    }
+}
