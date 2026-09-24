924c8cdefdeee690df1ae7926c900cc513d9a331
DanRM182 <ddaannn182@gmail.com>
2026-09-23 20:11:39 -0600

    Me equivoqué con el tipo de archivo, no me fijé que seleccioné una clase en lugar de interfaz

--- src/main/java/com/ramirezmontoya/tvmaze_middleware/repository/CommentRatingRepository.java
+++ src/main/java/com/ramirezmontoya/tvmaze_middleware/repository/CommentRatingRepository.java
@@ -1,5 +1,7 @@
 package com.ramirezmontoya.tvmaze_middleware.repository;
 
+import com.ramirezmontoya.tvmaze_middleware.document.CommentRatingDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class CommentRatingRepository extends MongoRepository<CommentRatingRepository, String> { }
+public interface CommentRatingRepository extends MongoRepository<CommentRatingDocument, String> {
+}
