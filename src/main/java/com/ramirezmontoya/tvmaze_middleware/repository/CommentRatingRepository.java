962dacda868d1f69e0e8fdff9b554e3d4dd02ef5
DanRM182 <ddaannn182@gmail.com>
2026-09-23 23:21:01 -0600

    Agregamos al Repository de comentarios-rating la búsqueda que nos retorne una colección de ShowIds

--- src/main/java/com/ramirezmontoya/tvmaze_middleware/repository/CommentRatingRepository.java
+++ src/main/java/com/ramirezmontoya/tvmaze_middleware/repository/CommentRatingRepository.java
@@ -3,5 +3,9 @@ package com.ramirezmontoya.tvmaze_middleware.repository;
 import com.ramirezmontoya.tvmaze_middleware.document.CommentRatingDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface CommentRatingRepository extends MongoRepository<CommentRatingDocument, String> {
+    List<CommentRatingDocument> findByShowIdIn(Collection<Long> showIds);
 }
