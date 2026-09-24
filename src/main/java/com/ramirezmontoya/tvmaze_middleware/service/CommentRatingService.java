60d5cec975e7cfcd8e320c2d48edbae9cbe9cf9a
DanRM182 <ddaannn182@gmail.com>
2026-09-23 23:43:29 -0600

    Concluimos el servicio para obtener los comment-rating mapeandolos en colección

--- src/main/java/com/ramirezmontoya/tvmaze_middleware/service/CommentRatingService.java
+++ src/main/java/com/ramirezmontoya/tvmaze_middleware/service/CommentRatingService.java
@@ -1,11 +1,18 @@
 package com.ramirezmontoya.tvmaze_middleware.service;
 
+import com.ramirezmontoya.tvmaze_middleware.document.CommentRatingDocument;
 import com.ramirezmontoya.tvmaze_middleware.dto.CommentRating.CommentRatingRequest;
+import com.ramirezmontoya.tvmaze_middleware.dto.CommentRating.CommentRatingResponse;
 import com.ramirezmontoya.tvmaze_middleware.mapper.CommentRatingMapper;
 import com.ramirezmontoya.tvmaze_middleware.repository.CommentRatingRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,5 +25,17 @@ public class CommentRatingService {
         log.info("Obtenemos y guardamos el documento");
 
         commentRatingRepository.save(commentRatingMapper.requestADocumento(commentRatingRequest));
+    }
+
+    public Map<Long, List<CommentRatingResponse>> obtenerComentariosRating(Collection<Long> showIds) {
+        if(showIds.isEmpty()) return Map.of();
+
+        log.info("Buscamos comentarios y ratings con los ShowId proveídos");
+
+        return commentRatingRepository.findByShowIdIn(showIds).stream()
+                .collect(Collectors.groupingBy(
+                        CommentRatingDocument::getShowId,
+                        Collectors.mapping(commentRatingMapper::documentoAResponse,
+                                Collectors.toList())));
     }
 }
